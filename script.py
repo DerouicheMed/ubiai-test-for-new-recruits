44860a4ecf613e12206da386e6e82b74e8dc34d9
derouichemed <derouiche.mo7amed@gmail.com>
2024-04-26 13:23:47 +0100

    feat branch 1 commit

--- script.py
+++ script.py
@@ -1,7 +1,15 @@
 import io
-import os
 import logging
+import os
 import traceback
+
 
 def this_function_is_in_main_branch():
     print("This function should be kept after merge")
+
+def this_function_is_in_feature_branch_1_only():
+    print("This function should be kept after merge with master but not with feature branch 2")
+
+def this_function_is_in_feature_branch():
+    print("This function should be kept after merge with master")
+
